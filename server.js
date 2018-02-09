8b29bbc0df04a394dfb4f4080790f6d8973641ff
Angular CLI <angular-cli@angular.io>
2018-02-09 13:23:33 -0500

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ server.js
@@ -0,0 +1,22 @@
+// Modules
+const path = require('path');
+const express = require('express');
+
+// App
+const app = express();
+
+// Run the app with static files from the /dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// For all GET requests, send back /dist/index.html
+// This passes routing to the Angular app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Set port
+const port = process.env.PORT || '1339';
+app.set('port', port);
+
+// Start the app
+app.listen(port, () => console.log(`Angular app deployed at localhost:${port}`));
