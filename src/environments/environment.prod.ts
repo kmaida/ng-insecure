7cf433f3f4bc4f7d0e627670c3e7cc04c1ef0501
Kim Maida <kim.maida@auth0.com>
2018-02-16 16:44:21 -0500

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   auth: {
     domain: 'kmaida.auth0.com',
     clientId: 'Rp1ZbPH6fK93lynaOt6HyQa3KcvN7Xl8',
-    redirect: 'http://localhost:1339/callback'
+    redirect: 'https://nginsecure.kmaida.net/callback'
   }
 };
