b49a2248bb57e4d889df7135b0f81d83281e4191
Kim Maida <kim.maida@auth0.com>
2018-02-20 14:58:32 -0500

    Add webserver security policies (to test in HTTPS environment)

--- server.js
+++ server.js
@@ -5,6 +5,27 @@ const express = require('express');
 // App
 const app = express();
 
+// Request security
+function reqSec(req, res, next) {
+  // HTTP Strict Transport Security (HSTS)
+  // Enforces HTTPS across the entire app
+  // While nginx does a redirect, HSTS redirects
+  // before anything is sent to the server
+  res.setHeader('Strict-Transport-Security', 'max-age=630720');
+  // Defend against Cross Site Scripting (XSS)
+  // This is when a malicious entity injects scripts
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  // Require iFrame sources to come from the same origin
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  // Content Security Policy
+  // Helpful in preventing XSS to ensure scripts only come
+  // from approved origins
+  res.setHeader('Content-Security-Policy', 'script-src "self"');
+  // Send the request on with security headers
+  return next();
+}
+app.use(reqSec);
+
 // Run the app with static files from the /dist directory
 app.use(express.static(__dirname + '/dist'));
 
