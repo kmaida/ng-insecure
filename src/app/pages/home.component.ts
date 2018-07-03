73de6b8244dd93925204d1e60b3125a0a102fc09
Kim Maida <kim.maida@auth0.com>
2018-07-03 15:59:33 -0400

    Clean up styles

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -4,9 +4,9 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   template: `
-  <app-auth></app-auth>
-  <h1 class="text-center">Welcome</h1>
-  <p class="lead container">Hello there! Welcome to our fun app.</p>
+    <app-auth></app-auth>
+    <h1 class="text-center">Welcome</h1>
+    <p class="lead container">Hello there! Welcome to our fun app.</p>
   `,
   styles: []
 })
